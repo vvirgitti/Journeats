e3d273ad3b04d32e7fdd4af9c22ca3bbb395af60
Tom Coakes <tomcoakes@gmail.com>
2015-03-24 18:00:57 +0000

    Google maps background and sidebar with search bar functioning

--- scripts/UIScripts.js
+++ scripts/UIScripts.js
@@ -1,26 +1,71 @@
-// The latitude and longitude of your business / place
-var position = [51.513977, -0.105572];
- 
-function showGoogleMaps() {
- 
-    var latLng = new google.maps.LatLng(position[0], position[1]);
- 
-    var mapOptions = {
-        zoom: 13, // initialize zoom level - the max value is 21
-        streetViewControl: false, // hide the yellow Street View pegman
-        scaleControl: true, // allow users to zoom the Google Map
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: latLng,
-        zoomControlOptions: {
-          position: google.maps.ControlPosition.RIGHT
-        },
-        panControlOptions: {
-          position: google.maps.ControlPosition.RIGHT
-        }
-    };
- 
-    map = new google.maps.Map(document.getElementById('googlemaps'),
-        mapOptions);
+function initialize() {
+
+  var markers = [];
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(51.517307, -0.073403),
+      new google.maps.LatLng(51.507307, -0.073403));
+  map.fitBounds(defaultBounds);
+
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+  
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+  // [END region_getplaces]
+
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
 }
- 
-google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
+google.maps.event.addDomListener(window, 'load', initialize);
