aa578d3c1c06c1514de7c43cec76b4eeb561f9ef
wardymate <chriswarddev@gmail.com>
2015-03-26 10:13:22 +0000

    API request to instagram with Lat and Long returns the list of matching instagram locations tests pass:

--- public/js/instagramController.js
+++ public/js/instagramController.js
@@ -4,6 +4,7 @@ journeats.controller('InstagramController', function($scope, $http, $resource) {
   $scope.instagramImages = [];
   $scope.instagramLocations = [];
 
+
   $scope.getInstagramLocations = function() {
 
     // var currentResource = $resource('https://api.instagram.com/v1/locations/search?lat=51.524697313&lng=-0.086685888&access_token=1787504160.d00f606.0c71ab52ffce47ca99505405e19ae2ae&callback=JSON_CALLBACK',{}, {
@@ -19,11 +20,14 @@ journeats.controller('InstagramController', function($scope, $http, $resource) {
 
 
 
-    $http({method: 'JSONP', url: 'https://api.instagram.com/v1/locations/search?callback=JSON_CALLBACK&lat=51.524697313&lng=-0.086685888&access_token=1787504160.d00f606.0c71ab52ffce47ca99505405e19ae2ae'})
-    .success(function(info) {
-      $scope.instagramLocations = info;
-      console.log(info.data);
+
+    $http.jsonp('https://api.instagram.com/v1/locations/search?lat=51.524697313&lng=-0.086685888&access_token=1787504160.d00f606.0c71ab52ffce47ca99505405e19ae2ae&callback=JSON_CALLBACK')
+    .then(function(response) {
+      console.log(response.data);
+      $scope.instagramLocations = response.data;
+
     });
+
 
 
 
