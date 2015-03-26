3b2c01966a59e86ad42b590280e5cf9e36962986
Tom Coakes <tomcoakes@gmail.com>
2015-03-26 10:34:07 +0000

    Fixed issue with ng-repeat not updating

--- public/js/searchResultsController.js
+++ public/js/searchResultsController.js
@@ -1,13 +1,5 @@
-journeats.controller('SearchResultsController', function($scope) {
-  
-  var searchResultsArray = [];
-  
-  searchResultsArray = [
-    {name: "Starbucks", priceRange: "$", aggregateScore: "86%"},
-    {name: "Costa Coffee", priceRange: "$$", aggregateScore: "84%"},
-    {name: "Ozone Coffee", priceRange: "$$$", aggregateScore: "72%"}
-  ];
+journeats.controller('SearchResultsController', function($rootScope, $scope) {
 
-  $scope.searchResults = searchResultsArray;
+  
 
 });
