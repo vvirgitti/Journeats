b6d96472e5e277a513de152d5c209a8fd75f773e
wardymate <chriswarddev@gmail.com>
2015-03-26 14:34:39 +0000

    converting API response to list of Instagram locations test fails

--- public/js/instagramController.js
+++ public/js/instagramController.js
@@ -2,10 +2,14 @@
 journeats.controller('InstagramController', function($scope, $http, $resource) {
 
   $scope.instagramImages = [];
-  $scope.instagramLocations = [];
+  $scope.instagramLocationList =[];
 
+  var searchTerm = $scope.searchTerm;
 
   $scope.getInstagramLocations = function() {
+
+  var latitude = $scope.latitude;
+  var longitude = $scope.longitude;
 
     // var currentResource = $resource('https://api.instagram.com/v1/locations/search?lat=51.524697313&lng=-0.086685888&access_token=1787504160.d00f606.0c71ab52ffce47ca99505405e19ae2ae&callback=JSON_CALLBACK',{}, {
     // query: {
@@ -20,17 +24,20 @@ journeats.controller('InstagramController', function($scope, $http, $resource) {
 
 
 
-
     $http.jsonp('https://api.instagram.com/v1/locations/search?lat=51.524697313&lng=-0.086685888&access_token=1787504160.d00f606.0c71ab52ffce47ca99505405e19ae2ae&callback=JSON_CALLBACK')
     .then(function(response) {
       console.log(response.data);
       $scope.instagramLocations = response.data;
-
     });
 
+  };
 
-
-
+  $scope.getlocationslist = function() {
+    $scope.getInstagramLocations();
+    // console.log($scope.instagramLocations);
+    $scope.instagramLocationList = $scope.instagramLocations.data.filter(function(item) {
+      return (item.name.indexOf(searchTerm) > -1);
+    })
   };
 
 });
