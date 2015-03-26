e2266b5196aeb18250cf25183ae66ef9a1a438ed
Tom Coakes <tomcoakes@gmail.com>
2015-03-26 16:55:03 +0000

    Name and address is now rendered on click in the bottom panel

--- public/js/searchResultsController.js
+++ public/js/searchResultsController.js
@@ -1,9 +1,10 @@
 journeats.controller('searchResultsController', function($rootScope, $scope, sharedProperties) {
 
   $scope.selectedResult = function(object) {
-    $scope.selectedObject = sharedProperties.getSelectedObject().filter(function(item) {
+    $scope.selectedObject = sharedProperties.getAllObjects().filter(function(item) {
       return (item === object);
     });
+    sharedProperties.setSelectedObject($scope.selectedObject);
     sharedProperties.setSelectedName($scope.selectedObject[0].name);
     sharedProperties.setSelectedLongitude($scope.selectedObject[0].geometry.location.k);
     sharedProperties.setSelectedLatitude($scope.selectedObject[0].geometry.location.D);
